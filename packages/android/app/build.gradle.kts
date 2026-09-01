0240092545233409ee304a2a3be03e3071e027e9
ChEnLeo-7 <136879424+ChEnLeo-7@users.noreply.github.com>
2026-09-01 08:06:02 +0000

    Release Android v0.10.1 redirect support

--- packages/android/app/build.gradle.kts
+++ packages/android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     applicationId = "app.musictogether.android"
     minSdk = 26
     targetSdk = 35
-    versionCode = 12
-    versionName = "0.10.0"
+    versionCode = 13
+    versionName = "0.10.1"
   }
 
   compileOptions {
