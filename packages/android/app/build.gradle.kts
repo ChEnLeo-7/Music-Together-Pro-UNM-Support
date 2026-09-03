b32489c2c7a87893d9779040a79ab464be8d7ade
ChEnLeo-7 <136879424+ChEnLeo-7@users.noreply.github.com>
2026-09-03 16:37:26 +0000

    Fix Android media notification build

--- packages/android/app/build.gradle.kts
+++ packages/android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     applicationId = "app.musictogether.android"
     minSdk = 26
     targetSdk = 35
-    versionCode = 15
-    versionName = "0.10.3"
+    versionCode = 16
+    versionName = "0.10.4"
   }
 
   compileOptions {
